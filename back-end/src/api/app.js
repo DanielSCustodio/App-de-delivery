ba56ecb702c305c470f9f87d1c19c6f8e8f03ceb
João Victor Paduam <joao.paduam@hotmail.com>
2022-06-01 15:01:27 -0300

    feat: add the register route on app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 
 const express = require('express');
 const loginRouter = require('../routers/loginRoute');
+const registerRouter = require('../routers/registerRoute');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(loginRouter);
+app.use(registerRouter);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
