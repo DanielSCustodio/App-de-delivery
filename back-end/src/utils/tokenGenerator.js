f60e64eaf130114db5e0e25a7561e102f30caa77
João Victor Paduam <joao.paduam@hotmail.com>
2022-06-01 15:02:51 -0300

    refactor: add fs to create secret key

--- back-end/src/utils/tokenGenerator.js
+++ back-end/src/utils/tokenGenerator.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 const jwtConfig = {
   expiresIn: '10d',
   algorithm: 'HS256',
 };
 
-const secret = process.env.JWT_SECRET;
+const secret = fs.readFileSync('jwt.evaluation.key', 'utf-8');
 
 const token = (data = {}) => jwt.sign({ data }, secret, jwtConfig);
 
