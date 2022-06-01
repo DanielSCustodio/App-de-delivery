409178e147d693843f95a4d78bfc79450bcd73eb
João Victor Paduam <joao.paduam@hotmail.com>
2022-06-01 20:04:26 -0300

    feat: create routes for login and home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+// import rockGlass from './images/rockGlass.svg';
+import LoginPage from './pages/login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
+    <Routes className="App">
+      <Route exact path="/login" element={ <LoginPage /> } />
+      <Route exact path="/" element={ <Navigate to="/login" /> } />
+      {/* <span className="logo">TRYBE</span> */}
+      {/* <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
         Glass
-      </object>
-    </div>
+      </object> */}
+    </Routes>
   );
 }
 
