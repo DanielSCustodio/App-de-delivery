fd895c6155f941f1dd7fed080b889d85d075e8a0
João Victor Paduam <joao.paduam@hotmail.com>
2022-06-03 17:14:16 -0300

    feat: add sale and seller routes in app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const loginRouter = require('../routers/loginRoute');
 const registerRouter = require('../routers/registerRoute');
 const productRouter = require('../routers/productRoute');
+const saleRouter = require('../routers/saleRoute');
+const sellerRouter = require('../routers/sellerRoute');
 
 const app = express();
 
@@ -13,6 +15,8 @@ app.use(cors());
 app.use(loginRouter);
 app.use(registerRouter);
 app.use(productRouter);
+app.use(saleRouter);
+app.use(sellerRouter);
 app.use('/images', express.static('public/images'));
 app.get('/coffee', (_req, res) => res.status(418).end());
 module.exports = app;
