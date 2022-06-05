b8663e8b490c16e448b3e9349a40255b0619c62d
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-06-04 21:05:12 -0300

    feat: add transactions to the service's createSale function

--- back-end/src/services/saleService.js
+++ back-end/src/services/saleService.js
@@ -1,24 +1,31 @@
-const { Sale } = require('../database/models');
+const Sequelize = require('sequelize');
 
-const createSale = async ({ 
-  userId,
-  sellerId,
-  totalPrice,
-  deliveryAddress,
-  deliveryNumber,
-  saleDate,
-}) => {
+const { Sale, SalesProduct } = require('../database/models');
+
+const config = require('../database/config/config');
+
+const sequelize = new Sequelize(config.development);
+
+const createSale = async (obj) => {
+  const { userId, sellerId, totalPrice: tPrice, deliveryAddress, deliveryNumber,
+    status, carts } = obj;
+
+  const totalPrice = Number(tPrice);
+
+  const result = await sequelize.transaction(async (t) => {
  const sale = await Sale.create({
-    userId,
-    sellerId,
-    totalPrice,
-    deliveryAddress,
-    deliveryNumber,
-    saleDate,
-    status: 'Pendente',
+      userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status,
+    }, { transaction: t });
+
+    await Promise.all(carts.map((cart) => SalesProduct.create(
+      { saleId: sale.id, productId: cart.id, quantity: cart.quantity }, { transaction: t },
+    )));
+
+    return ({ code: 201, sale });
   });
   
-  console.log(sale);
+  return result;
+};
 
   return ({ code: 201, sale });
 };
