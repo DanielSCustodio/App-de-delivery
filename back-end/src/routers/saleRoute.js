43de367835a7fd931e64d60b6bec0ecc501ed8bb
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-06-04 21:28:31 -0300

    feat:create get route to getSales and add tokenMid

--- back-end/src/routers/saleRoute.js
+++ back-end/src/routers/saleRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const saleController = require('../controllers/saleController');
 const saleMiddleware = require('../middlewares/saleMiddleware');
+const tokenMiddleware = require('../middlewares/tokenMiddleware');
 
 const saleRouter = express.Router();
 
 saleRouter.post('/sale', saleMiddleware, saleController.createSale);
+saleRouter.get('/sale', tokenMiddleware, saleController.getSales);
 
 module.exports = saleRouter;
