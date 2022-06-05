49444947810a7bdd2a5b48168f3c5255d0ab63c5
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-06-04 21:29:27 -0300

    fix: fix getSellers function return

--- back-end/src/controllers/sellerController.js
+++ back-end/src/controllers/sellerController.js
@@ -2,9 +2,9 @@ const sellerService = require('../services/sellerService');
 
 const getAllSellers = async (req, res, _next) => {
   try {
-    const { code, seller } = await sellerService.getSellers();
+    const { code, sellers } = await sellerService.getSellers();
 
-    return res.status(code).json(seller);
+    return res.status(code).json(sellers);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- back-end/src/services/sellerService.js
+++ back-end/src/services/sellerService.js
@@ -1,14 +1,12 @@
 const { User } = require('../database/models');
 
 const getSellers = async () => {
-  const [sellers] = await User.findAll({ where: { role: 'seller' } });
-  console.log(sellers);
-  const { id, name } = sellers;
-  const seller = {
-    id,
-    name,
-  };
-  return ({ code: 200, seller });
+  const sellers = await User.findAll({ 
+    where: { role: 'seller' },
+    attributes: { exclude: ['email', 'password', 'role'] },
+});
+
+  return ({ code: 200, sellers });
 };
 
 module.exports = {
