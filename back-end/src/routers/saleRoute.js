5d50ef0d4a969fdd5b104ad2410496e3ff5a65b3
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-06-04 21:22:15 -0300

    feat: add saleMiddleware in post saleRouter

--- back-end/src/routers/saleRoute.js
+++ back-end/src/routers/saleRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const saleController = require('../controllers/saleController');
+const saleMiddleware = require('../middlewares/saleMiddleware');
 
 const saleRouter = express.Router();
 
-saleRouter.post('/sale', saleController.createSale);
+saleRouter.post('/sale', saleMiddleware, saleController.createSale);
 
 module.exports = saleRouter;
