3f6550972dbd5ea39003d6c9ff0372fcfe22767a
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-06-07 21:24:44 -0300

    feat: re-adds saleMiddleware on saleRouter

--- back-end/src/routers/saleRoute.js
+++ back-end/src/routers/saleRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const saleController = require('../controllers/saleController');
-// const saleMiddleware = require('../middlewares/saleMiddleware');
+const saleMiddleware = require('../middlewares/saleMiddleware');
 const tokenMiddleware = require('../middlewares/tokenMiddleware');
 
 const saleRouter = express.Router();
-// saleMiddleware
-saleRouter.post('/sale', tokenMiddleware, saleController.createSale);
+
+saleRouter.post('/sale', tokenMiddleware, saleMiddleware, saleController.createSale);
 saleRouter.get('/sale', tokenMiddleware, saleController.getSales);
 
 module.exports = saleRouter;
