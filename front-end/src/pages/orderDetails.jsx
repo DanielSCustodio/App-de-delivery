d31398373e5a313fcbef1d584fffcfbdb507ed9a
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-06-08 21:27:35 -0300

    chore: adding orderDetails page

---
+++ front-end/src/pages/orderDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import CardHeader from '../templates/cardHeader';
+import CardSaleDetails from '../templates/cardSaleDetails';
+
+export default function OrderDetails() {
+  return (
+    <>
+      {/* <CardHeader /> */}
+      <CardSaleDetails />
+    </>
+  );
+}
