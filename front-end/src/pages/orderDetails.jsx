a846c5984073a1a9a82a69d585d65c957cf53ece
Camilla Del Guerso <camilladelguerso@gmail.com>
2022-06-09 00:15:56 -0300

    feat: add CarHearder on OrdersPage and OrderDetailsPage

--- front-end/src/pages/orderDetails.jsx
+++ front-end/src/pages/orderDetails.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-// import CardHeader from '../templates/cardHeader';
+import CardHeader from '../templates/cardHeader';
 import CardSaleDetails from '../templates/cardSaleDetails';
 
 export default function OrderDetails() {
+  const userData = JSON.parse(localStorage.getItem('user'));
+
   return (
     <>
-      {/* <CardHeader /> */}
+      <CardHeader
+        userName={ userData.name }
+      />
       <CardSaleDetails />
     </>
   );
--- front-end/src/pages/orders.jsx
+++ front-end/src/pages/orders.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import CardOrders from '../templates/cardOrders';
+import CardHeader from '../templates/cardHeader';
 
 export default function OrdersPage() {
+  const userData = JSON.parse(localStorage.getItem('user'));
+
   return (
-    <CardOrders />
+    <>
+      <CardHeader
+        userName={ userData.name }
+      />
+      <CardOrders />
+    </>
   );
 }
